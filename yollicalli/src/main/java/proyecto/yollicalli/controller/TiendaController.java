bf160bd5d4e2bb7739eebe89169c16f02e460dd5
Ollincatl <ollincatlj@gmail.com>
2024-03-21 23:08:46 -0600

    se modifico el servicio de buscaqueda de productos, y se agrego servicios para filtrar los productos

--- yollicalli/src/main/java/proyecto/yollicalli/controller/TiendaController.java
+++ yollicalli/src/main/java/proyecto/yollicalli/controller/TiendaController.java
@@ -1,11 +1,14 @@
 package proyecto.yollicalli.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import proyecto.yollicalli.model.Producto;
 import proyecto.yollicalli.model.ProductosResponse;
 import proyecto.yollicalli.service.ProductoService;
 
@@ -19,17 +22,49 @@ public class TiendaController {
 		this.productoService = productoService;
 	}
 	
-	@GetMapping(path = "/productos")
+	@GetMapping(path = "/productos") //http://localhost:8080/tienda/productos?pagina=1 otro link seria http://localhost:8080/tienda/productos?categorias=1&pagina=1
 	public ProductosResponse getProductos(
 			@RequestParam (required=false) String buscar,
-			@RequestParam (required=false) int pagina){
-		if(buscar != null) {
-			return productoService.getProductosBuscar(pagina,buscar);
-		}else {
-			return productoService.getAllProductos(pagina);
+			@RequestParam (required=false) String ordenPrecio,
+			@RequestParam (required=false) String ordenNombre,
+			@RequestParam (required=false) String categorias,
+			@RequestParam (required=false) Double precioMenor,
+			@RequestParam (required=false) Double precioMayor,
+			@RequestParam (required=true) int pagina){
+		ArrayList<Producto> tmpProductos =  null;
+		tmpProductos = productoService.getAllProductos();
+
+		if(ordenPrecio != null) {
+			tmpProductos=productoService.ordenProductosPrecio(tmpProductos,ordenPrecio);
+		}else{
+			System.out.println("no se aplico filtro");
 		}
 		
-	}
+		if(ordenNombre != null) {
+			tmpProductos=productoService.ordenProductosNombre(tmpProductos,ordenNombre);
+		}else {
+			System.out.println("no se aplico filtro");
+		}
+		
+		if(categorias != null) {
+			tmpProductos = productoService.filtrarCategorias(tmpProductos, categorias);
+		}else {
+			System.out.println("no se aplico filtro");
+		}
+		
+		if(precioMenor != null && precioMayor != null) {
+			tmpProductos = productoService.filtrarPrecios(tmpProductos, precioMenor, precioMayor);
+		}else {
+			System.out.println("no se aplico filtro");
+		}
+		
+		if(buscar != null) {
+			tmpProductos = productoService.getProductosBuscar(tmpProductos,buscar);
+		}
+		
+		return productoService.productosPaginacion(tmpProductos, pagina);
+		
+	}//getProductos
 
 }
 
