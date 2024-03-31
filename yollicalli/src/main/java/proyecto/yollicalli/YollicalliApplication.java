3d1217f400c48596295ec3f749d117ffbe4e47bf
ulises <ulises131994@gmail.com>
2024-03-31 10:48:26 -0600

    se realizan cambios para la conexion con front

--- yollicalli/src/main/java/proyecto/yollicalli/YollicalliApplication.java
+++ yollicalli/src/main/java/proyecto/yollicalli/YollicalliApplication.java
@@ -2,6 +2,9 @@ package proyecto.yollicalli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class YollicalliApplication {
@@ -9,4 +12,16 @@ public class YollicalliApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YollicalliApplication.class, args);
 	}//main
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Configura el acceso CORS para las rutas que empiezan con /api
+                        .allowedOrigins("http://127.0.0.1:5502") // Permite acceso desde http://127.0.0.1:5502
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite los métodos HTTP especificados
+                        .allowedHeaders("*"); // Permite todos los encabezados
+            }
+        };
+    }
 }
--- yollicalli/src/main/java/proyecto/yollicalli/model/Usuario.java
+++ yollicalli/src/main/java/proyecto/yollicalli/model/Usuario.java
@@ -13,60 +13,71 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
-    private int id;
+    @Column(name = "idUsuario", unique = true, nullable = false)
+    private Long idUsuario;
+
     @Column(nullable = false)
     private String nombre;
+
     @Column(nullable = false)
     private String correo;
+
     @Column(nullable = false)
     private String telefono;
+
     @Column(nullable = false)
     private String contrasenia;
-   //private static int total = 0;
+
     public Usuario(String nombre, String correo, String telefono, String contrasenia) {
         super();
         this.nombre = nombre;
         this.correo = correo;
         this.telefono = telefono;
         this.contrasenia = contrasenia;
-      //  Usuario.total++;
-       // id = Usuario.total;
     }
+
     public Usuario() {
-    //    Usuario.total++;
-      //  id = Usuario.total;
     }
-    public int getId() {
-        return id;
+
+    public Long getIdUsuario() {
+        return idUsuario;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getCorreo() {
         return correo;
     }
+
     public void setCorreo(String correo) {
         this.correo = correo;
     }
+
     public String getTelefono() {
         return telefono;
     }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
     public String getContrasenia() {
         return contrasenia;
     }
+
     public void setContrasenia(String contrasenia) {
         this.contrasenia = contrasenia;
     }
+
     @Override
     public String toString() {
-        return "Usuario [nombre=" + nombre + ", correo=" + correo + ", telefono=" + telefono + ", contrasenia=" + contrasenia
-                + ", id=" + id + "]";
+        return "Usuario [nombre=" + nombre + ", correo=" + correo + ", telefono=" + telefono + ", contrasenia="
+                + contrasenia + ", idUsuario=" + idUsuario + "]";
     }
 }
